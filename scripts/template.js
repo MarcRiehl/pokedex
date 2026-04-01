127f525959c0b9abaeba7b98a2457d375c757d57
MarcRiehl <210008956+MarcRiehl@users.noreply.github.com>
2026-04-01 15:27:34 +0200

    refactor: fetchDetailDataJson()

--- scripts/template.js
+++ scripts/template.js
@@ -1,7 +1,14 @@
 function getHTMLForThumbs(i) {
     return `
+    <div class="card">
     <div class="list-thumb">
-    <div class="fruits-output-name">${dataArrayPokemon[i].name}</div>
+    <h2 class="h2-card-title">${dataArrayPokemon[i].name}</h2>
+            <div class="list-thumb-img">
+            <div>
+            <img class="current-thumb-img" src="${dataArrayPokemon[i].sprites.other['official-artwork'].front_default}">
+            </div>
+    </div>
     </div>
     `;
 }
+
